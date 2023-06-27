e6c596402b3e2cf2f7ffc6e8f6f7896c422461fd
clinton <clinton.pillay@outlook.com>
2023-06-27 18:33:45 +0200

    locastack - downgraded to v1.4

--- Terradyno/script.py
+++ Terradyno/script.py
@@ -1,13 +1,16 @@
+import os
+import json
 import boto3
-import uuid
 import urllib
+import uuid
 
 
 endpointurl = "http://localstack:4566"
 
-#def script():
+
 def script(event,context):
 	clientd = boto3.client('dynamodb',region_name='us-east-1',endpoint_url=endpointurl)
+	#s3_client = boto3.client("s3", region_name = 'us-east-1')
 	bucket_name = event['Records'][0]['s3']['bucket']['name']
 	key = event['Records'][0]['s3']['object']['key']
 	key = urllib.parse.unquote_plus(key, encoding ='utf-8')
@@ -15,6 +18,8 @@ def script(event,context):
 	id_db = uuid.uuid4()
 	idval = str(id_db)
 	clientd.put_item(TableName='terratable',Item={'id':{'S':idval},'filename':{'S':key}})
-#	clientd.put_item(TableName='terratable',Item={'id':{'S':"uuid1"},'filename':{'S':"testfile1"}})
+#	clientd.put_item(TableName='terratable',Item={'id':{'S':"uuid"},'filename':{'S':"testfile"}})
 	return "success"
 
+#def script(event, context):
+#	return "upload success"
